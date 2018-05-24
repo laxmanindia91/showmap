5a6c4101c9182da0881527b2f71d2a28cfdc5cd0
Laxman <laxman@netscriptindia.com>
2018-05-24 11:53:19 +0530

    event work

--- simplemap.php
+++ simplemap.php
@@ -22,6 +22,7 @@
     <div id="map"></div>
     <script>
       var map;
+	  var infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
@@ -33,14 +34,39 @@
 		google.maps.event.addListener(map, 'zoom_changed', function() {
 		var zoomLevel = map.getZoom();
 		map.setCenter(myLatLng);
-		console.log(zoomLevel);
+		console.log('Zoom in/out: '+zoomLevel);
 		//infowindow.setContent('Zoom: ' + zoomLevel);
 		});
 		
-	google.maps.event.addListener(map, "position_changed", function() {	// Draggable event
-      var position = map.getPosition();
-	  console.log(position);
-    });
+		            //Zoom to 7 when clicked on marker
+            google.maps.event.addListener(map,'click',function() {
+               map.setZoom(9);
+			   console.log('clicked event');
+               //map.setCenter(map.getPosition());
+            });
+		
+		google.maps.event.addListener(map, "position_changed", function() {	// Draggable event
+		  var map_zoom = map.get('map').getZoom();
+		  console.log(map_zoom);
+		  
+		});
+		
+		
+		google.maps.event.addListener(map, 'drag', function(evt){
+			var map_center = map.getCenter();
+			console.log('dragged: '  + map_center);
+		});
+		
+		
+		// Get map in position changed event handler of marker
+		google.maps.event.addListener(map, 'click', function() {
+		var map1 = map.get('map');
+		var map_center = map.getCenter();
+		console.log('Clicked: '  + map_center);
+		});
+
+
+
 
       }
     </script>
